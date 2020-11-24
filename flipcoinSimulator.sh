e6dff557489eec22716e527b697120d62cd9d9cc
unknown <you@example.com>
2020-11-24 12:43:17 +0530

    Added New file

--- flipcoinSimulator.sh
+++ flipcoinSimulator.sh
@@ -1,17 +1,37 @@
 #!/bin/bash -x
 heads=0
 tails=0
-read n
-while [[ $n -ne 0 ]]
+while [[ $heads -le 21 || $tails -le 21 ]]
 do
+
         toss=$(($RANDOM%2))
-        if [ $toss -eq 1 ]
+        if [ $toss -eq 0 ]
         then
         heads=$(($heads+1))
-        else
+        elif [ $toss -eq 1 ]
+	then
         tails=$(($tails+1))
         fi
-        n=$(($n-1))
+
+	if [ $heads -eq 21 ]
+	then
+		echo " Head win 21 times"
+	x=$(($heads-$tails))
+	echo "head comes $X MORE TIMES than tail"
+	break
+
+	elif [ $tails -eq 21 ]
+	then
+		echo " head win  21 time"
+	y=$(($tails-$heads))
+	echo "tail comes $y more times than head"
+	break
+
+	elif [ $heads -eq $tails ]
+	then
+	echo "======Tie===="
+ 	break
+	fi
 done
 echo "wins of toss by heads call " $heads
 echo "wins of toss by tails call" $tails
